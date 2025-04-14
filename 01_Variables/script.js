fea8cdfb2eb855836af2137aba7f3de12f0129a6
Priyankaishwarkatti <155129187+Priyankaishwarkatti@users.noreply.github.com>
2025-04-14 14:28:53 +0530

    Add files via upload

---
+++ 01_Variables/script.js
@@ -0,0 +1,36 @@
+// 1. Variables & Data Types
+// Declare a variable that stores your name, age, and a boolean indicating if you're a student.
+let myName = 'Priyanka'
+let age = 24
+let isStudent = true
+const city = 'Pune'
+const Profession = 'Frontend Developer'
+
+// 2 .What is the difference between let, const, and var? Give examples.
+console.log(userAge)
+
+{
+  let firstName = 'Priya'
+}
+
+{
+  const lastName = 'Katti'
+}
+
+{
+  var userAge = 25
+}
+
+// let lastName = 'Katti'
+
+// lastName = "Ishwarkatti"
+
+const lastName = 'IshwarKatti'
+
+// lastName = "Ishwarkatti"
+
+// create selfintro using variables and template literals
+
+const myIntro = `Hello, My Name is ${
+  myName + ' ' + lastName} and I am ${age} Years old. I live in ${city}. I am a ${Profession}.`
+
