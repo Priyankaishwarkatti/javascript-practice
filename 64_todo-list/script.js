1010f683db76126ac2b4ae49570c7fac6b48a1c7
Priyankaishwarkatti <155129187+Priyankaishwarkatti@users.noreply.github.com>
2025-09-10 23:58:12 +0530

    Add files via upload

---
+++ 64_todo-list/script.js
@@ -0,0 +1,10 @@
+const todo = []
+
+todo.push("get up early")
+todo.push("go to running")
+todo.push("eat healthy")
+
+ for (i = 0; i < todo.length; i++){
+    console.log(`${i + 1} ${todo[i]}`);
+    // console.log(todo[i]);
+ }
