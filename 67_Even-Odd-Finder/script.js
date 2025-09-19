aad8fd81bb2e4bdff1ecc91824887a5a6c824a22
Priyankaishwarkatti <155129187+Priyankaishwarkatti@users.noreply.github.com>
2025-09-19 22:37:55 +0530

    Add files via upload

---
+++ 67_Even-Odd-Finder/script.js
@@ -0,0 +1,9 @@
+const arr = [1, 4, 3, 8, 5, 6]
+
+    for (let i=0; i < arr.length; i++){
+        if (arr[i] % 2 === 0){
+            console.log(`${arri} is Even.`);
+        } else {
+            console.log(`${i} is Odd.`);
+        }
+    }
