fe02fb64d0a90fa895999925f7cb16347849b40b
Priyankaishwarkatti <155129187+Priyankaishwarkatti@users.noreply.github.com>
2025-10-02 23:24:41 +0530

    Add files via upload

---
+++ 91_todo-list/script.js
@@ -0,0 +1,9 @@
+const arr = ['get up', 'eat', 'exercise', 'breakfast', 'study']
+
+function arrMethods() {
+    console.log(arr);
+    let arr1 = arr.push()
+    console.log(arr1);
+
+}
+arrMethods(...arr)
